58d2cea4eb241b57b90dacdf0623a0d103335dbd
Rajdeep Borgohain <150957746+rbgo404@users.noreply.github.com>
2024-03-28 17:15:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,14 @@
-import json
-from sentence_transformers import SentenceTransformer, util
+from FlagEmbedding import BGEM3FlagModel
 
 class InferlessPythonModel:
     def initialize(self):
-        self.model = SentenceTransformer("jinaai/jina-embeddings-v2-base-en",trust_remote_code=True)
-        # control your input sequence length up to 8192
-        self.model.max_seq_length = 1024
+        self.model = BGEM3FlagModel('BAAI/bge-m3', use_fp16=True)
 
     def infer(self, inputs):
         sentences = inputs["sentences"]
-        embeddings = self.model.encode(sentences)
-        return {"result": embeddings}
+        embeddings = self.model.encode(sentences, 
+                            batch_size=12, 
+                            max_length=8192)['dense_vecs']
+        return {"embeddings": list(embeddings)}
     def finalize(self, args):
         self.pipe = None
